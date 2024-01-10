dee443a24a7548c72a2cff4a56708e776c310c01
Theara Ya <theara.ya@outlook.com>
2024-01-09 22:15:49 -0500

    create and view memories

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,17 +1,48 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, flash
 from flask_login import current_user
 
+from .. import db
+from ..models import Memory
+from .forms import CreateMemoryForm
+
+
 bp = Blueprint('app', __name__)
+
 
 @bp.before_request
 def check_login():
     if not current_user.is_authenticated:
         return redirect(url_for('auth.login'))
 
+
 @bp.route('/')
 def index():
     return redirect(url_for('app.dashboard'))
 
+
 @bp.route('/dashboard')
 def dashboard():
     return render_template('app/dashboard.html')
+
+
+@bp.route('/create_memory', methods=['GET', 'POST'])
+def create_memory():
+    form = CreateMemoryForm()
+
+    if form.validate_on_submit():
+        memory = Memory(
+            prompt=form.prompt.data,
+            answer=form.answer.data,
+            user_id=current_user.id
+        )
+        db.session.add(memory)
+        db.session.commit()
+        flash('Memory created', category='success')
+        
+    return render_template('app/create_memory.html', form=form)
+
+
+@bp.route('/view_memories', methods=['GET'])
+def view_memories():
+    memories = Memory.query.filter_by(user_id=current_user.id).all()
+    return render_template('app/view_memories.html', memories=memories)
